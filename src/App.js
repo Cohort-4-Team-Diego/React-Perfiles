e549f0b43bfcc3fa2bfb28394d5bb6fbc8ed3f71
sanarguz <santiago.arcilag@gmail.com>
2020-07-27 21:15:38 -0500

    Primera versión del nuevo Hero

--- src/App.js
+++ src/App.js
@@ -5,69 +5,69 @@ import './assets/css/profile-picture.css'
 import './assets/css/footer.css'
 
 import Header from './components/Header'
+import Hero from './components/Hero.jsx'
 import TeamDistribution from './components/TeamDistribution'
 import TeamDistributionItem from './components/TeamDistributionItem'
 import Footer from './components/Footer'
 import ModalCrearPerfil from './components/ModalCrearPerfil.jsx'
 import ModalPerfil from './components/ModalPerfil.jsx'
-import api from './api';
+import api from './api'
 
 class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			data: [],
+			loading: true,
+			error: null,
+		}
+	}
 
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      loading: true,
-      error: null
-    }
-  }
+	componentDidMount() {
+		this.fetchData()
+	}
 
-  componentDidMount() {
-    this.fetchData();
-  }
+	fetchData = async () => {
+		this.setState({
+			loading: true,
+			error: null,
+		})
 
-  fetchData = async () => {
-    this.setState({
-      loading: true,
-      error: null
-    })
+		try {
+			const data = await api.profiles.list()
 
-    try {
-      const data = await api.profiles.list();
+			this.setState({
+				loading: false,
+				data,
+			})
+		} catch (error) {
+			this.setState({
+				loading: false,
+				error,
+			})
+		}
+	}
 
-      this.setState({
-        loading: false,
-        data
-      })
-    } catch (error) {
-      this.setState({
-        loading: false,
-        error
-      })
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-
-        <TeamDistribution>
-          {this.state.data.map((profile, index) => (
-            <TeamDistributionItem
-              profile={profile}
-              classPosition={`img-${index+1}`}
-              key={profile._id}
-            />
-          ))}
-        </TeamDistribution>
-        <ModalPerfil />
-        <ModalCrearPerfil />
-        <Footer />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<Hero />
+				<TeamDistribution>
+					{this.state.data.map((profile, index) => (
+						<TeamDistributionItem
+							profile={profile}
+							classPosition={`img-${index + 1}`}
+							key={profile._id}
+						/>
+					))}
+				</TeamDistribution>
+				<ModalPerfil />
+				<ModalCrearPerfil />
+				<Footer />
+			</div>
+		)
+	}
 }
 
 export default App
