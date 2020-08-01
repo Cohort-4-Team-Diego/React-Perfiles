9466ee86a1fd1691e53bccc5b33acb275887da7c
sanarguz <santiago.arcilag@gmail.com>
2020-07-31 21:29:09 -0500

    Updates

---
+++ precache-manifest.5177f4539c962932149b3cbb20f8abba.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76aee2bc59f2034c339b2416dd2bea81",
+    "url": "/React-Perfiles/index.html"
+  },
+  {
+    "revision": "20acfe87b7b5a181e9c0",
+    "url": "/React-Perfiles/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1cca59127f023d6ea6c2",
+    "url": "/React-Perfiles/static/css/main.9d4b95da.chunk.css"
+  },
+  {
+    "revision": "20acfe87b7b5a181e9c0",
+    "url": "/React-Perfiles/static/js/2.42aa22a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Perfiles/static/js/2.42aa22a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cca59127f023d6ea6c2",
+    "url": "/React-Perfiles/static/js/main.b1ff658b.chunk.js"
+  },
+  {
+    "revision": "c35a37f7947b2462a2dc",
+    "url": "/React-Perfiles/static/js/runtime-main.bccd29bb.js"
+  },
+  {
+    "revision": "9b9cc40a17f6648e3f04662eff671c1d",
+    "url": "/React-Perfiles/static/media/PanDevs-only-panda.9b9cc40a.png"
+  },
+  {
+    "revision": "28ae13b4564ec4a1ccb9e84cc3d03701",
+    "url": "/React-Perfiles/static/media/bamboo-pixel-6.28ae13b4.png"
+  },
+  {
+    "revision": "837149f69b0b75ee35f58fd86ae242b4",
+    "url": "/React-Perfiles/static/media/github-logo.837149f6.png"
+  },
+  {
+    "revision": "c3490b414bc06454e3e8822a31305fe2",
+    "url": "/React-Perfiles/static/media/icons8-email-80-gray.c3490b41.png"
+  },
+  {
+    "revision": "1c455d965e067db7827647429f246afb",
+    "url": "/React-Perfiles/static/media/icons8-email-80.1c455d96.png"
+  },
+  {
+    "revision": "5b7d98ad941cbedc00711e1153241833",
+    "url": "/React-Perfiles/static/media/icons8-github-100-gray.5b7d98ad.png"
+  },
+  {
+    "revision": "393afde9d3a46f5ee6ed992c227677ad",
+    "url": "/React-Perfiles/static/media/icons8-github-100.393afde9.png"
+  },
+  {
+    "revision": "144e541248c6533650ec605ece1e74f3",
+    "url": "/React-Perfiles/static/media/liana-bamboo-doble.144e5412.png"
+  },
+  {
+    "revision": "0886ee4ef064ecf92b1a2e5be0f812ab",
+    "url": "/React-Perfiles/static/media/mongo-logo-3.0886ee4e.png"
+  },
+  {
+    "revision": "25149b70a800c1538855fde951898d08",
+    "url": "/React-Perfiles/static/media/panda-placeholder.25149b70.webp"
+  },
+  {
+    "revision": "574476ae2c7fcb7473d12eae8d83e3f6",
+    "url": "/React-Perfiles/static/media/react-logo-2.574476ae.png"
+  }
+]);
